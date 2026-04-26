942954274beed862f54eba91071c092ca1d6aa9f
MhasnainGIT <mohdhasnain1544@gmail.com>
2026-04-26 22:31:23 +0530

    Fix: Added missing DropdownMenuGroup import

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -14,6 +14,7 @@ import { useUi } from '../../contexts/UiContext';
 import { cn } from '@/lib/utils';
 import {
   DropdownMenu,
+  DropdownMenuGroup,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
